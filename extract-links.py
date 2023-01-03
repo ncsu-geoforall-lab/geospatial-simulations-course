68c7fc4b5799311af0aa25dba4dc3c91c85cb46f
Vaclav Petras <wenzeslaus@gmail.com>
2023-01-03 11:10:12 -0500

    Ignore HTML comments in link extraction
    Link extraction for topics index now respects HTML comments in schedule.

--- extract-links.py
+++ extract-links.py
@@ -11,15 +11,23 @@ files = sys.argv[2:]
 
 # match any a even if the text part in on different line
 link = re.compile(r'<a href="({l})">'.format(l=link_pattern))
+comment_start = re.compile(r"^\s*<!--")
+comment_end = re.compile(r"-->\s*$")
 
 entries = []
 
 for name in files:
     with open(name, "r") as f:
+        in_comment = False
         for line in f:
-            match = link.search(line)
-            if match:
-                entries.append((match.group(1), name))
+            if comment_start.search(line):
+                in_comment = True
+            if not in_comment:
+                match = link.search(line)
+                if match:
+                    entries.append((match.group(1), name))
+            if comment_end.search(line):
+                in_comment = False
 
 # remove anchor part
 entries = [(entry.split("#")[0], origin) for entry, origin in entries]
